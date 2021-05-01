3cc5603c2aad94441ad62190ac6b1960583dae98
Steven Hair <stevenhair@users.noreply.github.com>
2021-05-01 11:21:45 -0400

    Improve generation of executable (#3)
    * we don't need to maintain a separate file
    
    * better help message
    
    * test the script portion

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
+import * as fs from 'fs/promises';
+
 import { createBadge } from './badge';
+import { processArguments } from './cli';
 import { getCoverageLevel } from './coverage';
 
 export async function generateBadge(input: string, label = 'coverage'): Promise<string> {
     const coverage = getCoverageLevel(input);
     return createBadge(label, coverage);
+}
+
+if (require.main === module) {
+    const args = processArguments();
+    generateBadge(args.input, args.label)
+        .then(async (badge) => fs.writeFile(args.output, badge))
+        .catch((e) => {
+            console.error(e);
+            process.exit(1);
+        });
 }
